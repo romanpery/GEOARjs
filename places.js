484a2ae1a414d27fe71d6d015404b83df332d211
Jose Alfredo <roman_pery@hotmail.com>
2019-12-28 19:05:36 -0600

    Quitando URL

--- places.js
+++ places.js
@@ -109,7 +109,7 @@ function renderPlaces(places) {
        // let text = document.createElement('a-link');
        // text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        // text.setAttribute('title', place.name);
-        text.setAttribute('href', 'http://www.example.com/');
+       // text.setAttribute('href', 'http://www.example.com/');
         // text.setAttribute('scale', '10 10 10');
 
 
