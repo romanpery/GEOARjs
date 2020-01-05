6e3ad99d4e645d05c44a45e1a7f4a6047f45af6e
Jose Alfredo <roman_pery@hotmail.com>
2020-01-05 12:44:58 -0600

    agregando coordenadas

--- places.js
+++ places.js
@@ -1,16 +1,15 @@
+
 window.onload = () => {
     let method = 'dynamic';
 
-    // if you want to statically add places, de-comment following line
-   // method = 'static';
-
+    // if you want to statically add places, de-comment following line:
+    // method = 'static';
     if (method === 'static') {
         let places = staticLoadPlaces();
-        renderPlaces(places);
+        return renderPlaces(places);
     }
 
     if (method !== 'static') {
-
         // first get current user location
         return navigator.geolocation.getCurrentPosition(function (position) {
 
@@ -33,39 +32,21 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: "Vecino",
+            name: "Rio",
             location: {
-                lat: 17.272236, // add here latitude if using static data
-                lng: -97.667063, // add here longitude if using static data
+                lat: 44.493271, // change here latitude if using static data
+                lng: 11.326040, // change here longitude if using static data
             }
         },
-
-        {
-            name: 'Vecino 2',
-            location: {
-                lat: 17.271983,
-                lng: -97.667155,
-            }
-        },
-
-        {
-            name: 'Rio',
-            location: {
-                lat: 17.272053,
-                lng: -97.666369,
-            }
-        }
     ];
 }
 
-
- 
 // getting places from REST APIs
 function dynamicLoadPlaces(position) {
     let params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',   // add your credentials here
-        clientSecret: '',   // add your credentials here
+        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
+        clientSecret: '',
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
@@ -96,44 +77,20 @@ function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
-        const latitude = place.location.lat;
-        const longitude = place.location.lng;
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
 
-        // add place icon
-        const icon = document.createElement('a-image');
-        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-        icon.setAttribute('name', place.name);
-        icon.setAttribute('src', '../assets/map-marker.png');
+        // add place name
+        let text = document.createElement('a-link');
+        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        text.setAttribute('title', place.name);
+        text.setAttribute('href', 'http://www.example.com/');
+        text.setAttribute('scale', '15 15 15');
 
-        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
+        text.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+        });
 
-        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-        const clickListener = function (ev) {
-            ev.stopPropagation();
-            ev.preventDefault();
-
-            const name = ev.target.getAttribute('name');
-
-            const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-            if (el && el === ev.target) {
-                const label = document.createElement('span');
-                const container = document.createElement('div');
-                container.setAttribute('id', 'place-label');
-                label.innerText = name;
-                container.appendChild(label);
-                document.body.appendChild(container);
-
-                setTimeout(() => {
-                    container.parentElement.removeChild(container);
-                }, 1500);
-            }
-        };
-
-        icon.addEventListener('click', clickListener);
-
-        scene.appendChild(icon);
+        scene.appendChild(text);
     });
 }
